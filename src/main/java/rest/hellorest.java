b1ed079b7bb970f8a94e8d1db99efea1cf226218
Anders W. Olsen <anders@wiberg.tech>
2017-06-10 23:47:35 +0200

    can lock pages

--- src/main/java/rest/hellorest.java
+++ src/main/java/rest/hellorest.java
@@ -1,16 +1,23 @@
 package rest;
 
+import auth.AuthenticationEndpoint;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 /**
  * Created by awo on 08/06/17.
  */
-
 @Path("hello")
 public class hellorest {
     @GET
-    public String test() {
+    @Path("insecure")
+    public String insecureData() {
         return "Hello World from rest!";
     }
+
+    @AuthenticationEndpoint.Secured
+    @GET
+    @Path("secure")
+    public String secureData() { return "this is secured data"; }
 }
