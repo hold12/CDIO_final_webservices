ebc0236816a884483ae326b654969634693bc870
Anders W. Olsen <anders@wiberg.tech>
2017-06-11 20:36:22 +0200

    added all methods

--- src/main/java/dao/TestUserDAO.java
+++ src/main/java/dao/TestUserDAO.java
@@ -33,4 +33,9 @@ public class TestUserDAO implements IUserDAO {
     public void updateUser(User user) throws DALException {
         throw new NotImplementedException();
     }
+
+    @Override
+    public User getFullUser(int userId) throws DALException {
+        throw new NotImplementedException();
+    }
 }
