0616cfe8cd03d090e0ad27afeb3fa44b989e32bc
Freya Helstrup <freyagh@me.com>
2017-06-12 09:54:28 +0200

    Implemented Recipe component DAO's and DTO

---
+++ src/main/java/dao/IRecipeComponentDAO.java
@@ -0,0 +1,13 @@
+package dao;
+
+import dto.RecipeComponent;
+import jdbclib.DALException;
+
+import java.util.List;
+
+public interface IRecipeComponentDAO {
+    RecipeComponent getRecipeComponent(int recipeId, int ingredientId) throws DALException;
+    List<RecipeComponent> getRecipeComponentList(int recipeId) throws DALException;
+    List<RecipeComponent> getRecipeComponentList() throws DALException;
+    void createRecipeComponent(RecipeComponent recipeComponent) throws DALException;
+}
