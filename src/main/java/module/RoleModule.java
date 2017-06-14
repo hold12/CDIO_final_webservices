4c2fe0557b62b66a1bd41e7c09dfe6b19660f098
Anders W. Olsen <anders@wiberg.tech>
2017-06-15 01:07:52 +0200

    Debug messages in rolemodule

---
+++ src/main/java/module/RoleModule.java
@@ -0,0 +1,50 @@
+package module;
+
+import auth.AuthenticationEndpoint;
+import config.Permission;
+import config.Routes;
+import dao.RoleDAO;
+import dto.Role;
+import jdbclib.DALException;
+import jdbclib.DBConnector;
+import jdbclib.DatabaseConnection;
+import jdbclib.IConnector;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by AndersWOlsen on 14-06-2017.
+ */
+@Path(Routes.MODULE_ROLE)
+public class RoleModule {
+    @AuthenticationEndpoint.Secured(Permission.ROLE_READ)
+    @POST
+    @Path(Routes.MODULE_ROLE_GET_ALL)
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Role> getAllRoles() {
+        System.out.println("Getting roles....");
+        try {
+            final IConnector db = new DBConnector(new DatabaseConnection());
+            RoleDAO roleDAO = new RoleDAO(db);
+            List<Role> roles = roleDAO.getRoleList();
+            System.out.print("Roles Count: ");
+            System.out.println(roles.size());
+            return roles;
+        } catch (DALException | IOException e) {
+            System.err.println("RoleModule: " + e.getMessage());
+            return null;
+        }
+    }
+
+    @GET
+    @Path("test")
+    public String test() {
+        return "Roles are working!!!! not..";
+    }
+}
